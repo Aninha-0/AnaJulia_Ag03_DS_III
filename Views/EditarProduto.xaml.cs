62290363d3b136bc8838c26b46e4bc587a736ee4
Ana Julia Nogueira <aninhands08@gmail.com>
2025-09-06 14:14:55 -0300

    Update EditarProduto.xaml.cs

--- Views/EditarProduto.xaml.cs
+++ Views/EditarProduto.xaml.cs
@@ -1,3 +1,6 @@
+using Anajulia_Ag3_DS_III.Models;
+using System.Threading.Tasks;
+
 namespace Anajulia_Ag3_DS_III.Views;
 
 public partial class EditarProduto : ContentPage
@@ -6,4 +9,28 @@ public partial class EditarProduto : ContentPage
 	{
 		InitializeComponent();
 	}
-}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Produto produto_anexado = BindingContext as Produto;
+
+            Produto p = new Produto
+            {
+                Id = produto_anexado.Id,
+                Descricao = txt_descricao.Text,
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Preco = Convert.ToDouble(txt_preco.Text)
+            };
+
+            await App.Db.Update(p);
+            await DisplayAlert("Sucesso!", "Registro Atualizado", "OK");
+            await Navigation.PopAsync();
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+}  
