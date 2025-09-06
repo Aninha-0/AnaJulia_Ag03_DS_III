225a3f10d07a17e58789d4f6d0aae1f980842f46
Ana Julia Nogueira <aninhands08@gmail.com>
2025-09-06 14:19:39 -0300

    Update Produto.cs

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,14 +1,29 @@
-﻿using SQLite;
+using SQLite;
 
 namespace Anajulia_Ag3_DS_III.Models
 {
     public class Produto
     {
+        string _descricao;
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
+        public string Descricao {
+            get => _descricao;
+            set
+            {
+              if(value == null)
+                {
+                    throw new Exception("Por favor, preencha a descrição");
+                }
+
+                _descricao = value;
+            }
+        }
         public double Quantidade { get; set; }
         public double Preco { get; set; }
+        public double Total { get => Quantidade * Preco; }
 
     }
 }
+
